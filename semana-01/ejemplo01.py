affec717e8ff9b3601d1b688bb000dce759c0b39
Brian Lara <brian.lara@outlook.com.ar>
2025-08-05 21:38:19 -0300

    Add example scripts for calculating average and variable assignments

---
+++ semana-01/ejemplo01.py
@@ -0,0 +1,18 @@
+# hacer un programa que ingrese la nota de 3 examenes y muestre el promedio
+
+# datos importantes
+
+# Datos de entrada
+# 3 notas
+nota1 = int(input("Ingrese nota 1: ")) ## convertimos el dato que ingresa (que es un texto) a entero
+nota2 = int(input("Ingrese nota 2: "))
+nota3 = int(input("Ingrese nota 3: "))
+# Operaciones
+# calcular promedio
+
+promedio = (nota1 + nota2 + nota3) / 3
+
+# Datos de salida
+# promedio
+print(promedio)
+
---
+++ semana-01/ejemplo02.py
@@ -0,0 +1,9 @@
+
+variable = "11"
+print(variable)
+
+variable = "Brian"
+print(variable)
+
+variable = 15
+print(variable)
